2bbcf862d1f68c9676e2aac8fe9a64e984daa1e1
jeffreybiles <bilesjeffrey@gmail.com>
2019-03-04 15:39:55 -0600

    SortableHeader component

---
+++ src/ui/components/sortable-header/component.js
@@ -0,0 +1,11 @@
+import Component from '@ember/component';
+import { action } from '@ember-decorators/object';
+
+export default class SortableHeaderComponent extends Component {
+  tagName = ''
+
+  @action
+  sortMyBooks(property, reversed){
+    this.sortBooks(property, reversed)
+  }
+}
