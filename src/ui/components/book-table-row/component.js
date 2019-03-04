6c66f6b75f316e33602f47bf2f3d874a30186a44
jeffreybiles <bilesjeffrey@gmail.com>
2019-03-04 17:03:04 -0600

    We can get rid of the in-line logic

--- src/ui/components/book-table-row/component.js
+++ src/ui/components/book-table-row/component.js
@@ -1,8 +1,14 @@
 import Component from '@ember/component';
 import { action } from '@ember-decorators/object';
+import { computed } from '@ember/object';
 
 export default class BookTableRowComponent extends Component {
   tagName = ''
+
+  @computed('selectedBookId', 'book')
+  get isSelected(){
+    return this.selectedBookId == this.book.id
+  }
 
   @action
   selectMyBook(){
