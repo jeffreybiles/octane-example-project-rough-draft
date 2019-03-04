5e17ee7ea2d44971ff4ce2fd5b795034f2a2bc9e
jeffreybiles <bilesjeffrey@gmail.com>
2019-03-04 15:50:41 -0600

    improved book select -- steb 3c, show it after first book selection

--- src/ui/routes/application/controller.js
+++ src/ui/routes/application/controller.js
@@ -17,40 +17,45 @@ export default class ApplicationController extends Controller {
     }
   }
 
-  // note: this is not the final way we're doing this.  Do not replicate data like this.
-  selectedBook = {
-    name: 'Brave New World',
-    author: 'Aldous Huxley',
-    publicationYear: 1932,
-    pages: 248,
+  selectedBookId = 1
+
+  @computed('selectedBookId', 'books')
+  get selectedBook(){
+    return this.books.findBy('id', this.selectedBookId)
   }
 
   books = [{
+    id: 1,
     name: 'Brave New World',
     author: 'Aldous Huxley',
     publicationYear: 1932,
     pages: 248,
   }, {
+    id: 2,
     name: 'The Way of Kings',
     author: 'Brandon Sanderson',
     publicationYear: 2010,
     pages: 1007,
   }, {
+    id: 3,
     name: 'Gulag Archipelago',
     author: 'Aleksandr Solzhenitsyn',
     publicationYear: 1973,
     pages: 2032,
   }, {
+    id: 4,
     name: 'A Fire Upon the Deep',
     author: 'Vernor Vinge',
     publicationYear: 1992,
     pages: 391,
   }, {
+    id: 5,
     name: 'Speaker for the Dead',
     author: 'Orson Scott Card',
     publicationYear: 1986,
     pages: 415
   }, {
+    id: 6,
     name: 'My Hero Academia volume 1',
     author: 'Kohei Horikoshi',
     publicationYear: 2014,
@@ -59,7 +64,7 @@ export default class ApplicationController extends Controller {
 
   @action
   selectBook(book){
-    this.set('selectedBook', book)
+    this.set('selectedBookId', book.id)
   }
 
   @action
