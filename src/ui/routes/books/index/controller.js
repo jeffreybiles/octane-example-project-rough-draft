97df441588dae7c916f856ea9e8a93b8f3f076ae
jeffreybiles <bilesjeffrey@gmail.com>
2019-03-06 21:36:57 -0600

    Transfer data to books.index route

--- src/ui/routes/books/index/controller.js
+++ src/ui/routes/books/index/controller.js
@@ -51,41 +51,6 @@ export default class BooksController extends Controller {
   @action selectAll(){ this.set('selectedBookIds', this.sortedBooks.mapBy('id')); }
   @action unselectAll(){ this.set('selectedBookIds', []); }
 
-  books = [{
-    id: 1,
-    name: 'Brave New World',
-    author: 'Aldous Huxley',
-    publicationYear: 1932,
-    pages: 248,
-  }, {
-    id: 2,
-    name: 'The Way of Kings',
-    author: 'Brandon Sanderson',
-    publicationYear: 2010,
-    pages: 1007,
-  }, {
-    id: 3,
-    name: 'Gulag Archipelago',
-    author: 'Aleksandr Solzhenitsyn',
-    publicationYear: 1973,
-    pages: 2032,
-  }, {
-    id: 4,
-    name: 'A Fire Upon the Deep',
-    author: 'Vernor Vinge',
-    publicationYear: 1992,
-    pages: 391,
-  }, {
-    id: 5,
-    name: 'Speaker for the Dead',
-    author: 'Orson Scott Card',
-    publicationYear: 1986,
-    pages: 415
-  }, {
-    id: 6,
-    name: 'My Hero Academia volume 1',
-    author: 'Kohei Horikoshi',
-    publicationYear: 2014,
-    pages: 192
-  }]
+  @computed('model.[]')
+  get books() { return this.model }
 }
